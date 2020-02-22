bbf50cb7bacc1dd01093d2b7c9ece1f6b8afb131
veronikaomrp <veronikaomrp@gmail.com>
2020-02-22 17:06:23 +0700

    commit

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+<?php
+ echo "Hello word";
+?>
+
 <html>
     <head>
         <title>  Biodata Saya</title>
             <body>
                 <h1>Biodata Saya</h1>
+                <h2>Selamat Datang ke dalam web site</h2>
             </body>
     </head>
 </html>
